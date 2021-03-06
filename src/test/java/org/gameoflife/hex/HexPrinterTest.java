a6e5fe45162e6e0b680d42029e299e30351c5da5
jmasonlee <jmasonlee@gmail.com>
2021-03-05 17:42:07 -0700

    r inline var

--- src/test/java/org/gameoflife/hex/HexPrinterTest.java
+++ src/test/java/org/gameoflife/hex/HexPrinterTest.java
@@ -11,9 +11,7 @@ class HexPrinterTest {
         for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 10; y++) {
                 if(Cell.isValid(x, y)){
-                    StringBuilder result1 = createTestOutputForBoardWithCellAtCoords(x, y);
-
-                    result.append(result1);
+                    result.append(createTestOutputForBoardWithCellAtCoords(x, y));
                 }
             }
         }
