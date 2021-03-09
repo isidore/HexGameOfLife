cdc331887591f139da8918b2155484239567a98d
jmasonlee <jmasonlee@gmail.com>
2021-03-09 07:05:33 -0700

    t Add test for new method

--- src/test/java/org/gameoflife/hex/CellTest.java
+++ src/test/java/org/gameoflife/hex/CellTest.java
@@ -3,6 +3,9 @@ package org.gameoflife.hex;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CellTest {
@@ -19,10 +22,18 @@ public class CellTest {
         GameOfLifeBoard gameOfLifeBoard = BoardTest.createBoardWithNeighbours(6, 0);
         Approvals.verify(HexPrinter.print(gameOfLifeBoard));
     }
+
     @Test
     void getLevelTwoNeighbours() {
         GameOfLifeBoard gameOfLifeBoard = BoardTest.createBoardWithNeighbours(0, 6);
         Approvals.verify(HexPrinter.print(gameOfLifeBoard));
     }
 
+    @Test
+    void testGetAllNeighbours() {
+        Cell cell = new Cell(4,4);
+        List<Cell> allNeighbours = new ArrayList<>(cell.getAllNeighbours());
+        GameOfLifeBoard gameOfLifeBoard = new GameOfLifeBoard(allNeighbours);
+        Approvals.verify(HexPrinter.print(gameOfLifeBoard));
+    }
 }
