5c2caa102763d5d54ba592bd5e2ff986e18f0c89
jmasonlee <jmasonlee@gmail.com>
2021-03-09 06:42:52 -0700

    r Make unstatic

--- src/main/java/org/gameoflife/hex/Cell.java
+++ src/main/java/org/gameoflife/hex/Cell.java
@@ -40,7 +40,7 @@ public class Cell {
         return cells;
     }
 
-    public static Set<Cell> getAllNeighbours(Cell cell) {
+    public Set<Cell> getAllNeighbours(Cell cell) {
         Set<Cell> temp = new HashSet<>();
         temp.addAll(cell.getLevelOneNeighbours());
         temp.addAll(cell.getLevelTwoNeighbours());
