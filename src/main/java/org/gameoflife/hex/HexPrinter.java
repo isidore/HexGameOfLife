5293d9837ecab45acb43749a9502936d2fc041c2
jmasonlee <jmasonlee@gmail.com>
2021-03-17 06:42:11 -0600

    r rename

--- src/main/java/org/gameoflife/hex/HexPrinter.java
+++ src/main/java/org/gameoflife/hex/HexPrinter.java
@@ -5,21 +5,21 @@ import dk.ilios.asciihexgrid.printers.SmallFlatAsciiHexPrinter;
 
 public class HexPrinter {
     public static String print(GameOfLifeBoard gameOfLifeBoard) {
-        AsciiBoard board = new AsciiBoard(0, 100, 0, 100, new SmallFlatAsciiHexPrinter());
+        AsciiBoard asciiBoard = new AsciiBoard(0, 100, 0, 100, new SmallFlatAsciiHexPrinter());
         int size = 10;
         for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
                 if (GameOfLifeBoard.Board.isValidCoordinate(x, y)) {
                     if (gameOfLifeBoard.board.isAlive(new Cell(x, y), gameOfLifeBoard)) {
-                        board.printHex( "X",  "X", 'X', translateX(x), translateY(x, y));
+                        asciiBoard.printHex( "X",  "X", 'X', translateX(x), translateY(x, y));
                     } else {
-                        board.printHex(x + "," + y, "", ' ', translateX(x), translateY(x, y));
+                        asciiBoard.printHex(x + "," + y, "", ' ', translateX(x), translateY(x, y));
 
                     }
                 }
             }
         }
-        return board.prettPrint(true);
+        return asciiBoard.prettPrint(true);
     }
 
     private static int translateY(int x, int y) {
