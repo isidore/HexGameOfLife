06e8ef278175a0dfad82844526b65670bde30d4f
jmasonlee <jmasonlee@gmail.com>
2021-03-19 06:55:31 -0600

    t create new variable

--- src/test/java/org/gameoflife/hex/BoardTest.java
+++ src/test/java/org/gameoflife/hex/BoardTest.java
@@ -11,6 +11,7 @@ class BoardTest {
     void testExceptionIsThrownForInvalidCoordinate() {
         try {
             GameOfLife board1 = new GameOfLife();
+            Board board = board1.board;
             board1.board.setAlive(1,2);
             fail("Coordinates were invalid, expected exception");
         }catch (Exception e) {
