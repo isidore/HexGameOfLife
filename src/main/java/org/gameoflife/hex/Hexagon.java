73f95a8f86191399daf0f7df8ec915ff7e9e9297
Practice <Practice@gmail.com>
2021-03-29 23:36:06 +0000

    r cleanup

--- src/main/java/org/gameoflife/hex/Hexagon.java
+++ src/main/java/org/gameoflife/hex/Hexagon.java
@@ -4,7 +4,7 @@ import org.lambda.query.Queryable;
 
 import java.awt.*;
 
-public class Hexagon  {
+public class Hexagon {
     private final int radius;
     private final int x;
     private final int y;
@@ -16,35 +16,25 @@ public class Hexagon  {
     }
 
     public Polygon getPolygon() {
-        int hexWidth = getHexWidth()/2;
-        int x = (this.x+1) * hexWidth;
-        int hexHeight = getHexHeight()/2;
+        int hexWidth = getHexWidth() / 2;
+        int x = (this.x + 1) * hexWidth;
+        int hexHeight = getHexHeight() / 2;
         int y = translateYGridToPixels(hexHeight);
         // 0 => 10
         // 1 => 25
         // 2 => 30
         // 3 => 45
-        int y2 = radius/2;
+        int y2 = radius / 2;
 
-        int[] pointsx = {x-hexWidth, x, x+hexWidth, x+hexWidth, x, x-hexWidth, x-hexWidth};
-        int[] pointsy = {y-y2, y-hexHeight, y-y2, y+y2, y+hexHeight, y+y2, y-y2};
+        int[] pointsx = {x - hexWidth, x, x + hexWidth, x + hexWidth, x, x - hexWidth, x - hexWidth};
+        int[] pointsy = {y - y2, y - hexHeight, y - y2, y + y2, y + hexHeight, y + y2, y - y2};
 
         Polygon polygon = new Polygon(pointsx, pointsy, 7);
-        return  polygon;
+        return polygon;
     }
 
     private int translateYGridToPixels(int hexHeight) {
-        switch (y) {
-            case 0:
-                return hexHeight * 1;
-            case 1:
-                return (int) (hexHeight * 2.5);
-            case 2:
-                return hexHeight * 4;
-            case 3:
-                return (int) (hexHeight * 5.5);
-        }
-        return (int)( hexHeight * (1+(1.5 * y)));//(int) (this.y / 2.0 + 1) * hexHeight;
+        return (int) (hexHeight * (1 + (1.5 * y)));
     }
 
     private int getHexHeight() {
@@ -53,23 +43,23 @@ public class Hexagon  {
 
     private int getHexWidth() {
 
-        return (int) (2 * radius * Math.sin(Math.PI*2/6));
+        return (int) (2 * radius * Math.sin(Math.PI * 2 / 6));
     }
 
     public Queryable<Point> getPoints() {
-        int hexWidth = getHexWidth()/2;
-        int x = (this.x+1) * hexWidth;
-        int hexHeight = getHexHeight()/2;
+        int hexWidth = getHexWidth() / 2;
+        int x = (this.x + 1) * hexWidth;
+        int hexHeight = getHexHeight() / 2;
         int y = translateYGridToPixels(hexHeight);
-        int y2 = radius/2;
+        int y2 = radius / 2;
 
-        return Queryable.as(new Point(x-hexWidth,y-y2),
-                new Point(x, y-hexHeight),
-                new Point(x+hexWidth,y-y2),
-                new Point(x+hexWidth, y+y2),
-                new Point(x,y+hexHeight),
-                new Point(x-hexWidth, y+y2),
-                new Point(x-hexWidth,y-y2)
+        return Queryable.as(new Point(x - hexWidth, y - y2),
+                new Point(x, y - hexHeight),
+                new Point(x + hexWidth, y - y2),
+                new Point(x + hexWidth, y + y2),
+                new Point(x, y + hexHeight),
+                new Point(x - hexWidth, y + y2),
+                new Point(x - hexWidth, y - y2)
         );
     }
 }
