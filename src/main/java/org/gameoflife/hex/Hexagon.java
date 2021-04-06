22dc687172fd026f7d5cd9f1dd0bf00ea4771b0c
jmasonlee <jmasonlee@gmail.com>
2021-04-06 05:59:09 -0600

    r rename

--- src/main/java/org/gameoflife/hex/Hexagon.java
+++ src/main/java/org/gameoflife/hex/Hexagon.java
@@ -52,15 +52,15 @@ public class Hexagon {
     public Queryable<Point> getPoints() {
         int halfHexWidth = getHexWidth() / 2;
         int halfHexHeight = getHexHeight() / 2;
-        int y2 = radius / 2;
+        int halfSideLength = radius / 2;
 
-        return Queryable.as(new Point(x - halfHexWidth, y - y2),
+        return Queryable.as(new Point(x - halfHexWidth, y - halfSideLength),
                 new Point(x, y - halfHexHeight),
-                new Point(x + halfHexWidth, y - y2),
-                new Point(x + halfHexWidth, y + y2),
+                new Point(x + halfHexWidth, y - halfSideLength),
+                new Point(x + halfHexWidth, y + halfSideLength),
                 new Point(x, y + halfHexHeight),
-                new Point(x - halfHexWidth, y + y2),
-                new Point(x - halfHexWidth, y - y2)
+                new Point(x - halfHexWidth, y + halfSideLength),
+                new Point(x - halfHexWidth, y - halfSideLength)
         );
     }
 }
