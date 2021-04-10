6d707b4c565699118499b9881491ad6282f9889c
jmasonlee <jmasonlee@gmail.com>
2021-04-10 11:49:54 -0600

    R use alternate constructor

--- src/test/java/org/gameoflife/hex/HexagonTest.java
+++ src/test/java/org/gameoflife/hex/HexagonTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 class HexagonTest {
     @Test
     void testY() {
-        Hexagon h1 = new Hexagon(10, 0, 0);
-        Hexagon h2 = new Hexagon(10, 0, 2);
+        Hexagon h1 = new Hexagon(10, new Coordinates(0,0));
+        Hexagon h2 = new Hexagon(10, new Coordinates(0,2));
        // Approvals.verifyAll("", Queryable.as(h1,h2), h -> ArrayUtils.toString(h.getPoints(), p -> String.format("(%s,%s)", p.x,p.y)));
     }
 }
