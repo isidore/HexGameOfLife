80d24777fa94290cf9fdd6eddd87a559f7ac0178
jmasonlee <jmasonlee@gmail.com>
2021-04-10 13:13:40 -0600

    r extract parameter

--- src/main/java/org/gameoflife/hex/GameOfLifePanel.java
+++ src/main/java/org/gameoflife/hex/GameOfLifePanel.java
@@ -16,11 +16,11 @@ public class GameOfLifePanel extends JPanel {
 
     public GameOfLifePanel(GameOfLife game) {
         this.game = game;
-        setSizeOfPanel();
+        setSizeOfPanel(BOARD_WIDTH - 1);
     }
 
-    private void setSizeOfPanel() {
-        Hexagon hexagon = new Hexagon(radius, new Coordinates(BOARD_WIDTH - 1, BOARD_HEIGHT - 1));
+    private void setSizeOfPanel(int width) {
+        Hexagon hexagon = new Hexagon(radius, new Coordinates(width, BOARD_HEIGHT - 1));
         Queryable<Point> points = hexagon.getPoints();
         int x = points.max(p -> p.x).x + 1;
         int y = points.max(p -> p.y).y + 1;
