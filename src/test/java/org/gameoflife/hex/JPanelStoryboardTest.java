f5c88ad1cc03a803a3e31ed22fe876c920275afd
jmasonlee <jmasonlee@gmail.com>
2021-04-23 08:20:29 -0600

    t Jpanel storyboarder

--- src/test/java/org/gameoflife/hex/JPanelStoryboardTest.java
+++ src/test/java/org/gameoflife/hex/JPanelStoryboardTest.java
@@ -1,6 +1,5 @@
 package org.gameoflife.hex;
 
-import org.approvaltests.Approvals;
 import org.approvaltests.awt.AwtApprovals;
 import org.approvaltests.reporters.ClipboardReporter;
 import org.approvaltests.reporters.UseReporter;
@@ -13,21 +12,44 @@ public class JPanelStoryboardTest {
 
     @Test
     @UseReporter(ClipboardReporter.class)
-    void testInitialJPanel(){
+    void testInitialJPanel() {
         JPanelStoryboard storyboard = new JPanelStoryboard();
-        storyboard.addPanel(new TestStoryPanel());
+        storyboard.addPanel(new TestStoryPanel(3));
+        storyboard.addPanel(new TestStoryPanel(2));
         AwtApprovals.verify(storyboard);
     }
 
     class TestStoryPanel extends JPanel {
+        int number;
+
+        public TestStoryPanel(int number) {
+            this.number = number;
+        }
+
         @Override
         public void paint(Graphics g) {
             super.paint(g);
             g.setColor(Color.ORANGE);
             g.fillRect(0, 0, this.getWidth(), this.getHeight());
+
             g.setColor(Color.BLUE);
-            g.fillRect(0, 0, this.getWidth()/2, this.getHeight()/2);
-            g.fillRect(getWidth()/2, getHeight()/2, getWidth(), getHeight());
+
+            int y = 0;
+
+            for (int i = 0; i < number; i++) {
+                int x = 0;
+                int height = y + this.getHeight() / number;
+                for (int j = 0; j < number; j++) {
+                    int width = x + this.getWidth() / number;
+
+                    if (i % 2 == j % 2) {
+                        g.fillRect(x, y, width, height);
+                    }
+
+                    x = width;
+                }
+                y = height;
+            }
         }
     }
 }
