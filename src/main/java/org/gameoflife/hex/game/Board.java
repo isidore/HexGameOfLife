3872d54ca3def9ba431fdf6ba81f6c74d3b24989
jmasonlee <jmasonlee@gmail.com>
2021-05-17 06:37:10 -0600

    r change access modifier

--- src/main/java/org/gameoflife/hex/game/Board.java
+++ src/main/java/org/gameoflife/hex/game/Board.java
@@ -8,19 +8,19 @@ import java.util.List;
 public class Board {
     private List<Cell> liveCells;
 
-    public List<Cell> getLiveCells() {
+    List<Cell> getLiveCells() {
         return liveCells;
     }
 
-    public void setLiveCells(List<Cell> liveCells) {
+    void setLiveCells(List<Cell> liveCells) {
         this.liveCells = liveCells;
     }
 
-    public void setAlive(int x, int y) {
+    void setAlive(int x, int y) {
         liveCells.add(new Cell(x, y));
     }
 
-    public boolean isAlive(Cell cell) {
+    boolean isAlive(Cell cell) {
         return liveCells.contains(cell);
     }
 
